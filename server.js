be0c2835badefbdffa9434c13836c8b9f75446a8
Amir41 <52294319+Amir41@users.noreply.github.com>
2019-07-04 14:11:14 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-const apiKey = '*****************';
+const apiKey = 'f6a0666696e8be61727e0660b737ab77';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
